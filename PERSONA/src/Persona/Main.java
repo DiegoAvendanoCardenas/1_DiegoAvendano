a12059df9fbe352a686e897f0beb57403f084779
Diego Alonso Avendaño Cardenas <111781924+DiegoAvendanoCardenas@users.noreply.github.com>
2023-09-12 09:46:29 -0500

    Add files via upload

---
+++ PERSONA/src/Persona/Main.java
@@ -0,0 +1,12 @@
+
+package Persona;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Persona objPersona = new Persona();
+        
+        objPersona.metodo01();
+        objPersona.metodo02();
+    }
+}
---
+++ PERSONA/src/Persona/Persona.java
@@ -0,0 +1,49 @@
+
+package Persona;
+
+public class Persona {
+    //Atributos
+    String nombre;
+    String sexo;
+    int edad;
+    
+    
+    
+    //Metodos / acciones
+    
+    public void hacerEjercicio(){
+        System.out.println("hago Ejercicios");
+    }
+    
+    public void caminar(){
+        System.out.println("camino un rato");
+    }
+    
+    public void metodo02(){
+    }
+    
+    //Sobrecarga de metodos
+    
+    
+    public void metodo01(){
+    }
+    
+    public void metodo01(int a){
+    }
+    
+    //Metdos que retornan
+    
+    public int año (){
+    return 15;
+    }
+    
+    public String Ciudad (){
+    return "Quilmana";
+    }
+    
+    public double peso (){
+    return 15.1;
+    }
+    
+    
+}
