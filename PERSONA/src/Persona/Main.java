e8b264e62e0eb9dbe51fdd84ec6fccf1e9994ee2
Diego Alonso Avendaño Cardenas <111781924+DiegoAvendanoCardenas@users.noreply.github.com>
2023-09-12 09:10:32 -0500

    Add files via upload

---
+++ PERSONA/src/Persona/Main.java
@@ -0,0 +1,15 @@
+
+package Persona;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Persona objPersona = new Persona();
+        
+        objPersona.nombre = "Diego";
+        
+        System.out.println(objPersona.nombre);
+        
+        
+    }
+}
